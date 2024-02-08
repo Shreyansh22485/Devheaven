348165e33f56f1868c38dc0b6eb4293c99121919
Saarthak Saxena <saarthak22421@iiitd.ac.in>
2024-02-09 02:31:39 +0530

    made second page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import Home from './pages/Home'
+import Result from './pages/Result' // Fix: Changed import statement to match correct file casing
 
 const App = () => {
   return (
-    <div><Home></Home></div>
+    // <div><Home></Home></div>
+    <div><Result></Result></div> // Fix: Corrected component name casing
   )
 }
 
---
+++ client/src/pages/Result.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import './Result.css';
+
+function Result() {
+  const pokemonImageSrc = 'image.png';
+  const bmi = 'Your BMI';
+  const bmiStatus = 'Your BMI Status';
+  const exercisesSuggested = 'Exercises Suggested for You';
+
+  return (
+    <div className="component-container">
+      <div className="content-container ">
+        <h2>You are like a:</h2>
+        <img src={pokemonImageSrc} alt="Your Pokémon" className="pokemon-image" />
+        <label className="bmi label">BMI: {bmi}</label>
+        <label className="bmi-status label">BMI Status: {bmiStatus}</label>
+        <label className="exercises-suggested label">Exercises Suggested: {exercisesSuggested}</label>
+      </div>
+    </div>
+  );
+}
+
+export default Result;
