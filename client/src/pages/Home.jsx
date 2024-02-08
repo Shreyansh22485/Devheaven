d157ee646f046ffb34a209d9be690007eef8ce89
Saarthak Saxena <saarthak22421@iiitd.ac.in>
2024-02-09 00:48:51 +0530

    did the fronted of the first page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import React from 'react';
+import './Home.css'; // Ensure you're importing your CSS file
+import { Button, TextInput } from 'flowbite-react';
+import { Label } from 'flowbite-react';
 
-const Home = () => {
+function Home() {
   return (
-    <div>Home</div>
-  )
+    <div className="flex flex-col flex-container">
+      <div className="rainbow-text">
+        <span><h1 className="curved-text">Which pokemon are you?</h1></span>
+      </div>
+      
+      <div className="flex flex-row label-input-container">
+      <Label className="fb-label" value="Height: ">Height:</Label>
+        <TextInput type="text" id="height" name="height" />
+      </div>
+      <div className="flex flex-row label-input-container">
+      <Label className="fb-label" value="Weight: ">Weight:</Label>
+        <TextInput type="text" id="weight" name="weight" />
+      </div>
+      <Button className="btn btn-primary">Submit</Button>
+    </div>
+  );
 }
 
-export default Home
+export default Home;
